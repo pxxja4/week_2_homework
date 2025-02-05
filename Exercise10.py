f2dad11ccd9690f7c359cda279e9acf5e8b6795e
Khrisha7 <khrisha.07@outlook.com>
2025-02-05 09:59:24 +0000

    Updated notes

--- Exercise10.py
+++ Exercise10.py
@@ -17,28 +17,30 @@ else:
 pattern = os.path.join(hdir,'*')
 # os.path.join is a function that joins different parts of the file path in an os independent way
 # the hdir part is the directory path where we are searching for the files
-#  the above asteriks is a wild card. It is saying to match all the files within this folder
+# the above asteriks is a wild card. It is saying to match all the files within this folder
+# Wildcards are special characters that represent one or more unknown characters and are useful for searching for patterns in text/filenames or databases --
+# --where you dont know the exact value but need to find something similar
 # essentially this string is telling python to look at all the files and folders inside the users home directory (C:\users\khris\ on my windows system
 #
 # TODO: Use the glob.glob() function to obtain the list of filenames
 file_list = glob.glob(pattern)
 # the glob.glob(pattern) looks for files and directories that match the wildcard pattern (*)
-# Since pattern = os.path.join(hdir, '*'), it's searching inside C:\Users\khris\.
-# glob.glob() returns a list of all matching files and folders
+# Since pattern = os.path.join(hdir, '*'), it's searching inside C:\Users\khris\. --
+# -- glob.glob() returns a list of all matching files and folders
 
 for file in file_list:
     print(file)
 # the file_list = glob.glob is returning a list of all the files in my directory
-#  the for in file_list is looping through my files, going through them one by one.
+# the "for in file_list:" is looping through my files, going through them one by one.
 
 # TODO: use os.path.getsize to find each file's size
 for file in file_list:
     file_size = os.path.getsize(file)
     print(f"file:{file}, size: {file_size}" )
-# the above for in file statement is looking at the files in the files list
+# the above "for file in file_list" statement is looking at the files in the files list
 # we are putting the os.path.getsize(file) function into the file_size variable
 # os.path.getsize finds the size of each of the files
-# the "f" in this line of code is an f string that allows you to put variables inside of a string using the curly braces
+# *the "f" in this line of code is an f string that allows you to put variables inside of a string using the curly braces
 # pyhon will replace what is in the curly brackets with the actual values of the variables
 
 # TODO: Add a test to only display files that are not zero length
@@ -49,10 +51,10 @@ for file in file_list:
 # TODO: Remove the leading directory name(s) from each filename before you print it -
 # using os.path.basename()
 # os.path.basename() extracts the basename of the file. the basename is the filename without the directory path
-#first introduce a for loop for the files in file_list to make the script run through the files within the entire file list
+# first introduce a for loop for the files in file_list to make the script run through the files within the entire file list
 for file in file_list:
     file_name =os.path.basename(file)
     #  we have put the os.path.basename(file) into the variable file_name
     print(f"file:{file_name}, size:{file_size}")
-# this has allowed us to print just the filenames without the directory path, making this more user-friendly
+# this has allowed us to print just the filenames without the directory path (\users\khris), making this more user-friendly
 #
--- python_demos_12.py
+++ python_demos_12.py
@@ -3,12 +3,16 @@
 i = 1
 j = 12
 while i < 42:
+    # while loops run as long as the condition is true and the condition is checked  before each iteration.
+    # while loops determines when to stop iterating
     i = i * 2
-    # the above line shows that i is doubled after going through the cycle each time till it is larger than the value of J (12) or whatever number you add at line 4
+    # the above line shows that i is doubled after going through the cycle each time till it is larger than the value of J (12) --
+    # --or whatever number you add at line 4
     if i > j:
      break
 #  break is used to "break" and exit from the loop when i > j therefore stopping the cycle
-#  if break is never reached, else will print ( loop expired) but is break is reached the "else" will be skipped (loop expired) and "final value" will be printed
+#  if break is not reached, else will print ( loop expired) but is break is reached the "else" will be skipped (loop expired) and --
+    #  ---"final value" will be printed
 # can only be nested for a while loop, not in a function or class definition within that loop
 
 # the else lines will print if the "break" statement is not reached
@@ -17,3 +21,9 @@ while i < 42:
 
 # if the break statement is reached then the below will print
 print("Final value: ", i)
+
+# Debugging pauses execution at breakpoints (lines you want to inspect).
+# Lets you step through the code line by line.
+# Shows variable values in real time.
+# Allows modifying variables on the fly to test different scenarios.
+# Identifies errors and exceptions with detailed messages.
